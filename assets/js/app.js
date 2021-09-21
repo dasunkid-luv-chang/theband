1524e4896ef7bfb88b44a20dd5f8eb963982ad0b
KhaIT <kha@gmail.com>
2021-09-21 12:06:52 +0700

    header mobile v2

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,19 +1,29 @@
 const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+let header = $('#header');
+let menu_items = $$('#nav > li:not(:last-child)');
 
 function preventReload(e){
     e.preventDefault();
 }
 
+function closeHeader(header){
+    header.style.height = '46px';
+}
+
+function openHeader(header){
+    header.style.height = 'auto';
+}
+
 $('#nav li:last-child').addEventListener('click',preventReload);
-
-let header = $('#header');
-console.log(header.clientHeight);
-
 $('#menu-bar').onclick = function() {
     //? check header có đang mở không
-    if(header.clientHeight == 46){
-        header.style.height = 'auto';
-    }
-    else
-        header.style.height = '46px';
+    (header.clientHeight == 46) ? openHeader(header): closeHeader(header);
 }
+
+menu_items.forEach(function(item){
+    item.onclick = function() {
+        closeHeader(header);
+    }
+})
+
